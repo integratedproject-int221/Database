ed1e559fa4180b216230dba7abee2f8a63be0f6b
Thongtong pp <56353861+Thongtong@users.noreply.github.com>
2021-05-10 15:02:41 +0700

    Update insert.sql

--- script/insert.sql
+++ script/insert.sql
@@ -16,24 +16,25 @@ insert into Brands values('hm','H&M');
 insert into Brands values('zr','Zara');
 
 
-insert into Products values('110101','Uniqlo U AIRism Cotton Oversized T-Shirt','T-Shirt','On-trend relaxed fit. Floral embroidery adds a vintage touch.','790.00','Male','2021-01-02','unq');
-insert into Products values('110201','JW ANDERSON Flower Embroidery T-Shirt','T-Shirt','The look of cotton and the feel of AIRism. Revolutionary item that balances design with functionality.','590.00','Female','2021-01-02','unq');
-insert into Products values('120101','J Supima Cotton Oversized Long Sleeve Shirt','Shirt','A premium cotton shirt in an oversized cut. Perfect for an on-trend, relaxed style.','1490.00','Male','2021-01-03','unq');
-insert into Products values('120201','Rayon Skipper Collar 3/4 Sleeve Blouse','Shirt','New skipper collar for ultimate beauty and comfort.','790.00','Female','2021-01-03','unq');
-insert into Products values('130101','Ultra Stretch Dry Sweat Pullover Hoodie','Sweatshirt','High-performance sweats for enhanced comfort, A versatile look that is perfect as everyday wear and sportswear.','990.00','Male','2021-01-05','unq');
-insert into Products values('130201','J Silk Cotton Crew Neck Long Sleeve Sweater','Sweatshirt','An elegant knit with a glossy sheen, In a light and easy design that you can also wear like a T-shirt.','2490.00','Female','2021-01-05','unq');
-insert into Products values('210101','Regular Fit T-shirt','T-Shirt',' T-shirt in soft, printed cotton jersey, Round neckline with a narrow trim','499.00','Male','2021-01-02','hm');
-insert into Products values('210201','Relaxed Fit T-shirt','T-Shirt','T-shirt in soft, printed cotton jersey, Relaxed fit with dropped shoulders and a round neckline with a narrow trim.','699.00','Female','2021-01-02','hm');
-insert into Products values('220101','Relaxed Fit Shirt Jacket','Shirt','Shirt jacket in washed cotton twill with a collar, Long sleeves with buttoned cuffs, flap chest pockets with a button and welt side pockets.','999.00','Male','2021-01-04','hm');
-insert into Products values('220201','Short-sleeved Poplin Shirt','Shirt','Shirt in cotton poplin with a collar, concealed buttons down the front, dropped shoulders. Pleat at the back, two open chest pockets.','499.00','Female','2021-01-04','hm');
-insert into Products values('230101','Sweatshirt fabric','Sweatshirt','Top in soft sweatshirt fabric in a slightly looser fit with dropped shoulders and ribbing around the neckline, cuffs and hem, Soft brushed inside.','499.00','Male','2021-01-06','hm');
-insert into Products values('230201','Regular Fit Hoodie','Sweatshirt','Long-sleeved hoodie in soft sweatshirt fabric with a kangaroo pocket, double-layered drawstring hood with a wrapover front.','999.00','Female','2021-01-06','hm');
-insert into Products values('310101','T-Shirt MICKEY MOUSE ©DISNEY','T-Shirt','T-shirt with a round neckline and short sleeves, printed with glitter coating pattern. MICKEY MOUSE ©DISNEY','990.00','Male','2021-01-07','zr');
-insert into Products values('310201','T-Shirt DONALD DUCK © DISNEY','T-Shirt','A loose T-shirt with a round neckline. Decorated with a check pattern on the front and a graphic print on the back. DONALD DUCK © DISNEY','1290.00','Female','2021-01-07','zr');
-insert into Products values('320101','Cotton-linen shirt','Shirt','A RELAXED FIT shirt made of cotton blend. Collar and short sleeves Open and close with a set of buttons.','1590.00','Male','2021-01-08','zr');
-insert into Products values('320201','OVERSIZE Linen Shirt','Shirt','A loose-fitting shirt in 100% linen. The collar and long sleeves are buttoned up at the cuffs. Side slit hem Front closure with buttons','1590.00','Female','2021-01-08','zr');
-insert into Products values('330101','Sweatshirt with zipper','Sweatshirt','Loose-fitting sweater with a stand collar, zip fastening, long sleeves and ribbed trim.','1590.00','Male','2021-01-09','zr');
-insert into Products values('330201','PREMIUM hoodie sweatshirt','Sweatshirt','A loose-fitting hoodie with a long hooded neck and long sleeves. Ventricular Ribbon trim edge','2190.00','Female','2021-01-09','zr');
+
+insert into Products values('110101','Uniqlo U AIRism Cotton Oversized T-Shirt','T-Shirt','On-trend relaxed fit. Floral embroidery adds a vintage touch.','790.00','Male',DATE('2021-01-02'),'unq');
+insert into Products values('110201','JW ANDERSON Flower Embroidery T-Shirt','T-Shirt','The look of cotton and the feel of AIRism. Revolutionary item that balances design with functionality.','590.00','Female',DATE('2021-01-02'),'unq');
+insert into Products values('120101','J Supima Cotton Oversized Long Sleeve Shirt','Shirt','A premium cotton shirt in an oversized cut. Perfect for an on-trend, relaxed style.','1490.00','Male',DATE('2021-01-03'),'unq');
+insert into Products values('120201','Rayon Skipper Collar 3/4 Sleeve Blouse','Shirt','New skipper collar for ultimate beauty and comfort.','790.00','Female',DATE('2021-01-03'),'unq');
+insert into Products values('130101','Ultra Stretch Dry Sweat Pullover Hoodie','Sweatshirt','High-performance sweats for enhanced comfort, A versatile look that is perfect as everyday wear and sportswear.','990.00','Male',DATE('2021-01-05'),'unq');
+insert into Products values('130201','J Silk Cotton Crew Neck Long Sleeve Sweater','Sweatshirt','An elegant knit with a glossy sheen, In a light and easy design that you can also wear like a T-shirt.','2490.00','Female',DATE('2021-01-05'),'unq');
+insert into Products values('210101','Regular Fit T-shirt','T-Shirt',' T-shirt in soft, printed cotton jersey, Round neckline with a narrow trim','499.00','Male',DATE('2021-01-02'),'hm');
+insert into Products values('210201','Relaxed Fit T-shirt','T-Shirt','T-shirt in soft, printed cotton jersey, Relaxed fit with dropped shoulders and a round neckline with a narrow trim.','699.00','Female',DATE('2021-01-02'),'hm');
+insert into Products values('220101','Relaxed Fit Shirt Jacket','Shirt','Shirt jacket in washed cotton twill with a collar, Long sleeves with buttoned cuffs, flap chest pockets with a button and welt side pockets.','999.00','Male',DATE('2021-01-04'),'hm');
+insert into Products values('220201','Short-sleeved Poplin Shirt','Shirt','Shirt in cotton poplin with a collar, concealed buttons down the front, dropped shoulders. Pleat at the back, two open chest pockets.','499.00','Female',DATE('2021-01-04'),'hm');
+insert into Products values('230101','Sweatshirt fabric','Sweatshirt','Top in soft sweatshirt fabric in a slightly looser fit with dropped shoulders and ribbing around the neckline, cuffs and hem, Soft brushed inside.','499.00','Male',DATE('2021-01-06'),'hm');
+insert into Products values('230201','Regular Fit Hoodie','Sweatshirt','Long-sleeved hoodie in soft sweatshirt fabric with a kangaroo pocket, double-layered drawstring hood with a wrapover front.','999.00','Female',DATE('2021-01-06'),'hm');
+insert into Products values('310101','T-Shirt MICKEY MOUSE ©DISNEY','T-Shirt','T-shirt with a round neckline and short sleeves, printed with glitter coating pattern. MICKEY MOUSE ©DISNEY','990.00','Male',DATE('2021-01-07'),'zr');
+insert into Products values('310201','T-Shirt DONALD DUCK © DISNEY','T-Shirt','A loose T-shirt with a round neckline. Decorated with a check pattern on the front and a graphic print on the back. DONALD DUCK © DISNEY','1290.00','Female',DATE('2021-01-07'),'zr');
+insert into Products values('320101','Cotton-linen shirt','Shirt','A RELAXED FIT shirt made of cotton blend. Collar and short sleeves Open and close with a set of buttons.','1590.00','Male',DATE('2021-01-08'),'zr');
+insert into Products values('320201','OVERSIZE Linen Shirt','Shirt','A loose-fitting shirt in 100% linen. The collar and long sleeves are buttoned up at the cuffs. Side slit hem Front closure with buttons','1590.00','Female',DATE('2021-01-08'),'zr');
+insert into Products values('330101','Sweatshirt with zipper','Sweatshirt','Loose-fitting sweater with a stand collar, zip fastening, long sleeves and ribbed trim.','1590.00','Male',DATE('2021-01-09'),'zr');
+insert into Products values('330201','PREMIUM hoodie sweatshirt','Sweatshirt','A loose-fitting hoodie with a long hooded neck and long sleeves. Ventricular Ribbon trim edge','2190.00','Female',DATE('2021-01-09'),'zr');
 
 
 
